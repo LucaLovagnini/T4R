a91e3cdc532fb18cbf2e871d322525063a5df2d4
llovagn <llovagn@amazon.es>
2018-10-08 00:23:09 +0200

    Using TextSwitcher, fixed image

--- app/src/main/java/com/example/llovagn/t4r/MainActivity.kt
+++ app/src/main/java/com/example/llovagn/t4r/MainActivity.kt
@@ -1,33 +1,28 @@
 package com.example.llovagn.t4r
 
+import android.content.Context
+import android.graphics.Color
 import android.media.Image
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
 import android.view.View
+import android.widget.TextView
 import com.example.llovagn.t4r.model.CircularModel
 import com.example.llovagn.t4r.model.Model
 import com.example.llovagn.t4r.presenter.Presenter
 import com.example.llovagn.t4r.presenter.PresenterImpl
 import com.example.llovagn.t4r.view.ViewMVC
-import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
-import android.R.id.edit
-import android.content.Context
-import android.preference.PreferenceManager
-import android.content.SharedPreferences
-import com.google.gson.reflect.TypeToken
-import android.content.Context.MODE_PRIVATE
-import android.graphics.Color
-import java.io.FileOutputStream
 import java.io.ObjectInputStream
 import java.io.ObjectOutputStream
+import java.util.*
+
 
 class MainActivity : AppCompatActivity(), ViewMVC {
 
     private lateinit var presenter: Presenter
-    private var model : Model? = CircularModel(LinkedList<State>(Arrays.asList(
+    private var model: Model? = CircularModel(LinkedList<State>(Arrays.asList(
             State("First"),
             State("Second"),
             State("Third"),
@@ -39,7 +34,20 @@ class MainActivity : AppCompatActivity(), ViewMVC {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if(savedInstanceState != null ) {
+        mainTextView.setFactory {
+            val textView = TextView(this@MainActivity)
+            textView.textSize = 36f
+            textView.gravity = Gravity.CENTER
+            textView.setTextColor(Color.WHITE)
+            textView.setShadowLayer(30f, 0f, 0f, Color.GRAY);
+            textView
+        }
+
+        mainTextView.setInAnimation(this, android.R.anim.fade_in);
+        mainTextView.setOutAnimation(this, android.R.anim.fade_out);
+
+
+        if (savedInstanceState != null) {
             model = savedInstanceState.getParcelable("model")
         }
         presenter = PresenterImpl(this, model!!)
@@ -54,7 +62,7 @@ class MainActivity : AppCompatActivity(), ViewMVC {
     override fun onPause() {
         super.onPause()
 
-        val context= applicationContext;
+        val context = applicationContext;
 
         val fos = context.openFileOutput("model.ser", Context.MODE_PRIVATE)
         val os = ObjectOutputStream(fos)
@@ -65,7 +73,7 @@ class MainActivity : AppCompatActivity(), ViewMVC {
 
     override fun onResume() {
         super.onResume()
-        val context= applicationContext;
+        val context = applicationContext;
         val file = context.getFileStreamPath("model.ser")
 
         if (file == null || !file.exists()) {
@@ -90,7 +98,7 @@ class MainActivity : AppCompatActivity(), ViewMVC {
     }
 
     override fun setMainTextView(message: String) {
-        mainTextView.text = message
+        mainTextView.setText(message)
     }
 
     override fun playSong(state: State) {
